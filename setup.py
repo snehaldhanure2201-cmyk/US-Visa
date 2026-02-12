775c1f7babd8036a1980d90e58058eae952c9788
snehaldhanure2201-cmyk <snehaldhanure2201@gmail.com>
2026-02-12 21:33:17 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Snehal",
+    author_email="snehaldhanure2201@gmail.com",
+    packages=find_packages()
+)
