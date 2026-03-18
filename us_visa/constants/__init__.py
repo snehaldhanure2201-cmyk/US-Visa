d15b6491f2d3d27747490a3c137f7f8b4d8ebb47
snehaldhanure2201-cmyk <snehaldhanure2201@gmail.com>
2026-03-18 17:24:43 +0530

    prediction and app added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -24,6 +24,13 @@ PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -62,4 +69,14 @@ MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2201"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
 
